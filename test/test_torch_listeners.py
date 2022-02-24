390f53decae0397c87ed65d4953a897a7a234e4a
Suguru Imanaga <suguru_imanaga@cysista.co.jp>
2022-02-24 15:13:29 +0900

    Add TensorBoardLossReporter

---
+++ test/test_torch_listeners.py
@@ -0,0 +1,26 @@
+import unittest
+from pathlib import Path
+
+from tools.torch.listeners import TensorBoardLossReporter
+
+log_dir = Path(__file__).parent.joinpath('log')
+
+
+class TestTorchTensorBoardLossReporter(unittest.TestCase):
+
+    def test(self):
+        reporter = TensorBoardLossReporter(log_dir=log_dir)
+        reporter.start()
+
+        train_loss = val_loss = 100
+
+        for i in range(1, 101):
+            data = type('data', (object,), {
+                'train_loss': train_loss,
+                'val_loss': val_loss
+            })
+            train_loss -= 5 / i
+            val_loss -= 4 / i
+            reporter.post_epoch(i, data, None)
+
+        reporter.end()
---
+++ tools/torch/listeners.py
@@ -0,0 +1,31 @@
+import datetime
+from pathlib import Path
+
+from torch.utils.tensorboard import SummaryWriter
+
+TAG = 'loss'
+
+
+# TODO イテレーションごとの方がよいか？
+class TensorBoardLossReporter:
+
+    def __init__(self, log_dir: Path):
+        now = datetime.datetime.now()
+        folder = now.strftime('%Y%m%d-%H%M%S')
+        self.log_dir = log_dir.joinpath(folder)
+        self.train_writer = None
+        self.val_writer = None
+
+    def start(self):
+        train_dir = self.log_dir.joinpath('train')
+        val_dir = self.log_dir.joinpath('val')
+        self.train_writer = SummaryWriter(log_dir=train_dir)
+        self.val_writer = SummaryWriter(log_dir=val_dir)
+
+    def post_epoch(self, epoch, data, target):
+        self.train_writer.add_scalar(TAG, data.train_loss, epoch)
+        self.val_writer.add_scalar(TAG, data.val_loss, epoch)
+
+    def end(self):
+        self.train_writer.close()
+        self.val_writer.close()
